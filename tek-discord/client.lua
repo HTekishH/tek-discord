d622980b221010be19838bc6794a2e1212401235
HTekishH <64609867+HTekishH@users.noreply.github.com>
2021-05-05 19:25:32 +0200

    Create client.lua

---
+++ tek-discord/client.lua
@@ -0,0 +1,10 @@
+-- Scripted by Tekish --
+
+RegisterCommand('discord', function()
+        msg("DISCORDLINKHERE") -- Replace with your server invite
+    end
+end, false)
+
+function msg(text)
+    TriggerEvent("chatMessage", "[Discord]", {140, 158, 255}, text)
+end
